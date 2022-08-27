ba3411bd2d9bd10ebf182d3652cbcd2b41d63181
Yuvraj Singh Rajawat <69616079+yuvraj1464@users.noreply.github.com>
2022-08-27 10:24:49 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+            
+            <p> </p>
         </div>
     )
 }
